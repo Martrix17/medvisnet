e013db7298519b16fc62be1a025371a1ced3f47d
Martrix17 <martin.gajek45@gmail.com>
2025-10-12 23:57:13 +0000

    chore: update pyproject.toml readme and dependecy fields and simplified setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,4 @@
-from pathlib import Path
+from setuptools import setup
 
-from setuptools import find_packages, setup
-
-this_dir = Path(__file__).parent
-long_description = (
-    (this_dir / "README.md").read_text() if (this_dir / "README.md").exists() else ""
-)
-
-setup(
-    name="medvisnet",
-    version="0.1.0",
-    author="Martrix17",
-    description="Medical computer vision project with DL and DevOps.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
-    python_requires=">=3.9",
-    install_requires=[],
-)
+if __name__ == "__main__":
+    setup()
